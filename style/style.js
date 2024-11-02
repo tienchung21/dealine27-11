291f5fc0f36006f67fe2ab3386067dea7d6266ae
tienchung21 <chung9atm@gmail.com>
2024-11-02 13:55:33 +0700

    chungcuto

---
+++ style/style.js
@@ -0,0 +1,37 @@
+function on1(){
+    document.getElementById("mywork-modal1").style.display="flex"
+}
+function off1(){
+    document.getElementById("mywork-modal1").style.display="none"
+}
+function on2(){
+    document.getElementById("mywork-modal2").style.display="flex"
+}
+function off2(){
+    document.getElementById("mywork-modal2").style.display="none"
+}
+
+function on3(){
+    document.getElementById("mywork-modal3").style.display="flex"
+}
+function off3(){
+    document.getElementById("mywork-modal3").style.display="none"
+}
+function on4(){
+    document.getElementById("mywork-modal4").style.display="flex"
+}
+function off4(){
+    document.getElementById("mywork-modal4").style.display="none"
+}
+function on5(){
+    document.getElementById("mywork-modal5").style.display="flex"
+}
+function off5(){
+    document.getElementById("mywork-modal5").style.display="none"
+}
+function on6(){
+    document.getElementById("mywork-modal6").style.display="flex"
+}
+function off6(){
+    document.getElementById("mywork-modal6").style.display="none"
+}
